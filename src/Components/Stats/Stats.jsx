88faa53a6e12ea01bed78331a25a20018a8bf7d8
Woutsthew <nekita351@gmail.com>
2022-12-14 11:24:25 +0400

    добавил статистику по претензиям и не выполненных задачах

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -3,7 +3,7 @@ import styles from './Stats.module.scss';
 
 const pages = {
   start: '/',
-  kpdByTech: 'КПД техников',
+  kpdByTech: 'Статистика техников',
   objectsWithoutSim: 'Объекты без сим карт',
 
 }
@@ -41,16 +41,39 @@ const Stats = () => {
 
   const [startDate, setStartDate] = useState([]);
   const [endDate, setEndDate] = useState([]);
+  
   const [kpds, setKpds] = useState([]);
+  const [pretension, setPretension] = useState([]);
+  const [notComleted, setNotCompleted] = useState([]);
 
   const [objects, setObjects] = useState([]);
   const [res, setRes] = useState([]);
 
 
+  const StatsTechs = () => {
+    GetKpdByTech();
+    GetPretension();
+    GetNotComleted();
+  }
+
   const GetKpdByTech = () => {
     fetch(`https://volga24bot.com/kartoteka/api/tech/daily/getKpdByTech.php?startDate=${startDate}&endDate=${endDate}`)
       .then(res => res.json())
-      .then(data => setKpds(data.items))
+      .then(data => setKpds(data.items.sort((a, b) => a.tech.localeCompare(b.tech))))
+      .catch(e => console.log(e))
+  }
+
+  const GetPretension = () => {
+    fetch(`https://volga24bot.com/kartoteka/api/tech/daily/getPretension.php?startDate=${startDate}&endDate=${endDate}`)
+      .then(res => res.json())
+      .then(data => setPretension(data.items))
+      .catch(e => console.log(e))
+  }
+
+  const GetNotComleted = () => {
+    fetch(`https://volga24bot.com/kartoteka/api/tech/daily/getNotCompleted.php?startDate=${startDate}&endDate=${endDate}`)
+      .then(res => res.json())
+      .then(data => setNotCompleted(data.items))
       .catch(e => console.log(e))
   }
 
@@ -76,21 +99,34 @@ const Stats = () => {
           <div className={styles.chooseDate}>
             <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} />
             <input type="date" value={endDate} onChange={e => setEndDate(e.target.value)} />
-            <button onClick={GetKpdByTech}>Принять</button>
+            <button onClick={StatsTechs}>Принять</button>
           </div>
           <div>
             <table border="1" cellpadding="3">
               <tr>
                 <th>Техники</th>
+                <th>Претензии</th>
+                <th>Не выполненые</th>
                 {Array.from(new Set(kpds?.map(el => el.createAt))).map(el => <th>{el}</th>)}
               </tr>
               {Array.from(new Set(kpds?.map(el => el.tech))).map(el =>
-                <>
-                  <tr>
-                    <th>{el.split(' ')[0]}</th>
-                    {kpds?.filter(kpd => kpd.tech === el).map(kpd => <th>{kpd.kpd}</th>)}
-                  </tr>
-                </>)}
+                <tr>
+                  <th>{el.split(' ')[0]}</th>
+                  <th>
+                    <span style={{ "color": "green" }}>
+                      {pretension.filter(p => p.plane_techs.includes(el) && p.typeResult === 'Выполнена').length}
+                    </span>
+                    -
+                    <span style={{ "color": "red" }}>
+                      {pretension.filter(p => p.plane_techs.includes(el) && p.typeResult !== 'Выполнена').length}
+                    </span>
+                  </th>
+                  <th>
+                    {notComleted.filter(p => p.plane_techs.includes(el)).length}
+                  </th>
+                  {kpds?.filter(kpd => kpd.tech === el).map(kpd => <th><span style={kpd.kpd >= 70 ? { "color": "green" } : kpd.kpd <= 20 ? { "color": "red" } : null}>{kpd.kpd}</span></th>)}
+                </tr>
+              )}
             </table>
           </div>
         </>
