bbf075f5ce9949fcca1e2d8407147ef13800ba07
Woutsthew <nekita351@gmail.com>
2023-01-24 08:16:08 +0400

    статистика тех службы

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -67,8 +67,7 @@ const Stats = () => {
       {/* <PageButton onClick={() => setPage(pages.objectsWithoutSim)}>{pages.objectsWithoutSim}</PageButton> */}
 
       {page === pages.kpds
-        ?
-        <>
+        ? <>
           <div className={styles.chooseDate}>
             <input type="date" valueAsDate={startDate} onChange={e => setStartDate(new Date(e.target.value))} />
             <input type="date" valueAsDate={endDate} onChange={e => setEndDate(new Date(e.target.value))} />
@@ -79,75 +78,7 @@ const Stats = () => {
             <td><TableKpd sd={startDate} ed={endDate} kpds={stats?.kpds} /></td>
             <td><TableStats sd={startDate} ed={endDate} stats={stats} /></td>
           </table>
-          {/* {countNotPlanedAndRepeats.cnp ?
-            <span>Не распределенных задач: {countNotPlanedAndRepeats.cnp}</span>
-            : null} */}
-          {/* Введомость за {startDate.toLocaleString('ru', { month: 'long' })}
-          <div>
-            <table border="1" cellpadding="3">
-              <tr>
-                <th>Техники</th>
-                <th>Претензии</th>
-                {countNotPlanedAndRepeats.items
-                  ? <th>Повторы</th>
-                  : null}
-                <th>Не выполненые</th>
-                <th>Среднее</th>
-                <th>Мин</th>
-                <th>Макс</th>
-                {Array.from(new Set(kpds?.map(el => el.createAt))).map(el =>
-                  (new Date(el).getDay() === 6 || new Date(el).getDay() === 0)
-                    ? <th><span style={{ "color": "blue" }}>{el}</span></th>
-                    : <th>{el}</th>)}
-              </tr>
-              {Array.from(new Set(kpds?.map(el => el.tech))).map(el =>
-                <tr>
-                  <th>{el.split(' ')[0]}</th>
-                  <th>
-                    <span style={{ "color": "green" }}>
-                      {pretension.filter(p => p.plane_techs.includes(el) && p.typeResult === 'Выполнена').length}
-                    </span>
-                    -
-                    <span style={{ "color": "red" }}>
-                      {pretension.filter(p => p.plane_techs.includes(el) && p.typeResult !== 'Выполнена').length}
-                    </span>
-                  </th>
-                  {countNotPlanedAndRepeats.items
-                    ? <th>
-                      <span style={{ "color": "green" }}>
-                        {countNotPlanedAndRepeats.items.filter(p => p.plane_techs.includes(el) && p.typeResult === 'Выполнена').length}
-                      </span>
-                      -
-                      <span style={{ "color": "red" }}>
-                        {countNotPlanedAndRepeats.items.filter(p => p.plane_techs.includes(el) && p.typeResult !== 'Выполнена').length}
-                      </span>
-                    </th>
-                    : null}
-                  <th>
-                    {notComleted.filter(p => p.plane_techs.includes(el)).length}
-                  </th>
-                  <th>
-                    {Average(kpds.filter(item => item.tech === el)
-                      .filter(item => (new Date(item.createAt).getDay() !== 6
-                        && new Date(item.createAt).getDay() !== 0) || item.kpd !== '0'))}
-                  </th>
-                  <th>
-                    {Math.min(...kpds.filter(item => item.tech === el)
-                      .filter(item => (new Date(item.createAt).getDay() !== 6
-                        && new Date(item.createAt).getDay() !== 0) || item.kpd !== '0').map(item => +item.kpd))}
-                  </th>
-                  <th>
-                    {Math.max(...kpds.filter(item => item.tech === el)
-                      .filter(item => (new Date(item.createAt).getDay() !== 6
-                        && new Date(item.createAt).getDay() !== 0) || item.kpd !== '0').map(item => +item.kpd))}
-                  </th>
-                  {kpds?.filter(kpd => kpd.tech === el).map(kpd => <th><span style={kpd.kpd >= 70 ? { "color": "green" } : kpd.kpd <= 20 ? { "color": "red" } : null}>{kpd.kpd}</span></th>)}
-                </tr>
-              )}
-            </table>
-          </div> */}
-        </>
-        : null}
+        </> : null}
 
       {/* {page === pages.objectsWithoutSim
         ? <div className={styles.objects}>
--- src/Components/Stats/TableStats/TableStats.jsx
+++ src/Components/Stats/TableStats/TableStats.jsx
@@ -4,12 +4,26 @@ const TableStats = ({ sd, ed, stats }) => {
   const techs = Array.from(new Set(stats?.kpds?.map(el => el.tech)));
   const days = Array.from(new Set(stats?.kpds?.map(el => el.createAt)));
 
-  const columns = ['ФИО', 'Сред', 'Макс', 'Мин', 'Претензии', 'Повторы', 'Не выполненые', 'Прогулы'];
-  const Average = (tech, arr) => {
-    const filtredArr = arr.filter(item => item.tech === tech)
+  const colorKpd = (kpd) => {
+    if (+kpd >= 70) return { 'background-color': 'lightgreen' };
+    if (+kpd <= 30) return { 'background-color': 'lightsalmon' };
+    return { 'background-color': 'white' };
+  }
+
+  const filtredArray = (tech, arr) => {
+    return arr.filter(item => item.tech === tech)
       .filter(item => (new Date(item.createAt).getDay() !== 6
         && new Date(item.createAt).getDay() !== 0) || item.kpd !== '0');
+  }
+
+  const columns = ['ФИО', 'Сред', 'Макс', 'Мин', 'Претензии', 'Повторы', 'Не выполненные', 'Прогулы'];
+  const Average = (tech, arr) => {
+    const filtredArr = filtredArray(tech, arr);
     return (filtredArr.reduce((a, b) => a + +b.kpd, 0) / filtredArr.length).toFixed(1);
+  }
+  const Absense = (tech, arr) => {
+    const filtredArr = filtredArray(tech, arr);
+    return filtredArr.filter(item => item.kpd === '0').length;
   }
 
   return (
@@ -18,7 +32,15 @@ const TableStats = ({ sd, ed, stats }) => {
       {techs.map(tech =>
         <tr>
           <th>{tech}</th>
-          <th>{Average(tech, stats?.kpds)}</th>
+          <th style={colorKpd(Average(tech, stats?.kpds))}>{Average(tech, stats?.kpds)}</th>
+          <th style={colorKpd(Math.max(...filtredArray(tech, stats?.kpds).map(item => +item.kpd)))}>
+            {Math.max(...filtredArray(tech, stats?.kpds).map(item => +item.kpd))}</th>
+          <th style={colorKpd(Math.min(...filtredArray(tech, stats?.kpds).map(item => +item.kpd)))}>
+            {Math.min(...filtredArray(tech, stats?.kpds).map(item => +item.kpd))}</th>
+          <th>{stats?.pretensions.filter(p => p.plane_techs.includes(tech)).length}</th>
+          <th>{stats?.repeats.filter(p => p.plane_techs.includes(tech)).length}</th>
+          <th>{stats?.notCompleted.filter(p => p.plane_techs.includes(tech)).length}</th>
+          <th>{Absense(tech, stats?.kpds)}</th>
         </tr>)}
     </table>
   )
